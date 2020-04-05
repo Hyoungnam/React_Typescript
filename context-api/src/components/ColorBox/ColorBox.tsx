c8f37304e3c35e6e9f8e196c8d4a19f3ab7e0aca
South <kimhyoungnams@gmail.com>
2020-04-05 23:01:57 +0900

    useContext

--- context-api/src/components/ColorBox/ColorBox.tsx
+++ context-api/src/components/ColorBox/ColorBox.tsx
@@ -1,19 +1,25 @@
 import * as React from "react";
-import  { ColorConsumer }  from "../../contexts/color"
+import { useContext } from "react";
+import ColorContext, { ColorConsumer }  from "../../contexts/color"
 
 {/* 이러한 패턴을 Function as a child. 혹은 Render Props라고 한다.  */}
 {/* 컴포넌트의 childrend이 있어야 할 자리에 일반 jsx 혹은 문자열이 아닌 함수를 전달하는 것 */}
 const ColorBox: React.FC = () => {
+  const { state } = useContext(ColorContext);
   return (
     // consumer에서는 value 이름을 자유롭게 마음대로 사용해도 괜찮다
-    <ColorConsumer>
-      {value => (
-        <>
-        <div style={{width: "64px", height: "64px", background: value.state.color}}/>           
-        <div style={{width: "64px", height: "64px", background: value.state.subColor}}/>           
-        </>
-      )}
-    </ColorConsumer>
+    // <ColorConsumer>
+      // {/* {value => (
+      //   <>
+      //     <div style={{width: "64px", height: "64px", background: value.state.color}}/>           
+      //     <div style={{width: "64px", height: "64px", background: value.state.subColor}}/>           
+      //   </>
+      // )} */}
+    // {/* </ColorConsumer> */}
+      <>
+        <div style={{width: "64px", height: "64px", background: state.color}}/>           
+        <div style={{width: "64px", height: "64px", background: state.subColor}}/>           
+      </>
   )
 }
 export default ColorBox;
--- context-api/src/components/ColorBox/SelectColors.tsx
+++ context-api/src/components/ColorBox/SelectColors.tsx
@@ -14,7 +14,7 @@ const SelectColors = () => {
               <div 
                 key={color}
                 style={{background: color, width: "24px", height: "24px", cursor: "pointer"}}
-                onClick={()=> value.actions.setColor(color)}
+                onClick={ () => value.actions.setColor(color) }
                 // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
                 onContextMenu={ e => {
                   // 메뉴 뜨지 않음
